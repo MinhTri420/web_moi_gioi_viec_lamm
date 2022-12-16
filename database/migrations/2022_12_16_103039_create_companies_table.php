850bf8c52db97857292dca2978af03dfa89c6acd
MinhTri420 <dinhminhtri420@gmail.com>
2022-12-16 19:21:07 +0700

    16.12

---
+++ database/migrations/2022_12_16_103039_create_companies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('address');
+            $table->string('address2');
+            $table->string('district');
+            $table->string('city');
+            $table->string('country');
+            $table->string('zipcode');
+            $table->string('email');
+            $table->string('logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};
---
+++ database/migrations/2022_12_16_104815_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('avatar')->nullable();
+            $table->string('email');
+            $table->string('password');
+            $table->string('phone')->nullable();
+            $table->string('link')->nullable();
+            $table->string('role')->default(1);
+            $table->text('bio')->nullable();
+            $table->string('position')->nullable();
+            $table->boolean('gender')->nullable();
+            $table->string('city');
+            $table->foreignId('company_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
